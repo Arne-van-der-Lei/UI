e579fb4b21116dfc9f445fb0328e2092950495e5
arne.vanderlei <arne.vanderlei@E50419.jozi.loc>
2015-02-06 15:42:48 +0100

    hallo

---
+++ prijslijst/src/database/DB.java
@@ -0,0 +1,61 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package database;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author arne.vanderlei
+ */
+public class DB {
+
+    final static String DBPAD = "src/database/DP.mdb";
+    final static String DB = "jdbc:odbc:Driver={Microsoft Access Driver (*.mdb)};DBQ=" + DBPAD;
+    public Connection con;
+
+    public DB() {
+
+        try {
+            Class.forName("sun.jdbc.odbc.JdbcOdbcDriver");
+        } catch (ClassNotFoundException e) {
+            System.out.println("Error: " + e);
+        }
+
+        try {
+            con = DriverManager.getConnection(DB, "", "");
+        } catch (SQLException e) {
+            System.out.println("Error: " + e);
+        }
+
+    }
+
+    public Map<String, Float> getString() {
+        Map<String, Float> ret = new HashMap<String, Float>();
+
+        try {
+            Statement s = con.createStatement();
+            ResultSet rs = s.executeQuery("select * from producten");
+            if (rs != null) {
+                while (rs.next()) {
+                    ret.put( rs.getString("Productnaam"),rs.getFloat("Prijs"));
+                }
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(DB.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return ret;
+
+    }
+}
